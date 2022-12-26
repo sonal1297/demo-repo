e4c680159daa696b1ea34bcec82a8635030944d2
sonal1297 <56485128+sonal1297@users.noreply.github.com>
2022-12-26 22:49:50 +0530

    Update upload-notebook.sh

--- upload-notebook.sh
+++ upload-notebook.sh
@@ -1,3 +1,7 @@
 wget "https://raw.githubusercontent.com/sonal1297/demo-repo/main/upload-notebook1.py"
-pip3 install numpy
+python3 -m pip install --upgrade pip
+pip3 install azureml-core
+pip3 install --upgrade azureml-core
+pip3 install azureml-dataset-runtime --upgrade
+az account show
 python upload-notebook1.py
